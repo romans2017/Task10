f2d96e37dc396ea315654b710904a2452380e2e2
romans2017 <savchuk.r0@gmail.com>
2021-09-04 20:31:52 +0300

    done 1 and 2

--- src/main/java/StreamTasks.java
+++ src/main/java/StreamTasks.java
@@ -8,7 +8,12 @@ public class StreamTasks {
         return list.stream().parallel().map(item -> item * 2).filter(item -> item % 10 != 2).collect(Collectors.toList());
     }
 
+    public static List<String> excludeZ(List<String> list) {
+        return list.stream().parallel().filter(item->!item.contains("z")&&!item.contains("Z")).collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
-        System.out.println(doubling(List.of(1,3,4,2,3)));
+        System.out.println(doubling(List.of(1,3,4,2,12,11)));
+        System.out.println(excludeZ(List.of("asdsa", "aszsa", "asewrwsa", "asZsa")));
     }
 }
